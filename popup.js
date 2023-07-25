170369eadcf9d5ac7874dde699643409cd83639f
Merter <90144140+Sugarchild@users.noreply.github.com>
2023-07-25 12:10:55 +0300

    add student/teacher pages
    and add some properties

---
+++ popup.js
@@ -0,0 +1,149 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("myModal1");
+    var butt = document.getElementById("mybutt1");
+    var span = document.getElementsByClassName("close")[0];
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+    var modal = document.getElementById("myModal2");
+    var butt = document.getElementById("mybutt2");
+    var span = document.getElementsByClassName("close")[0];
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+    var modal = document.getElementById("myModal3");
+    var butt = document.getElementById("mybutt3");
+    var span = document.getElementsByClassName("close")[0];
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+    var modal = document.getElementById("myModal4");
+    var butt = document.getElementById("mybutt4");
+    var span = document.getElementsByClassName("close")[0];
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+    var modal = document.getElementById("myModal5");
+    var butt = document.getElementById("mybutt5");
+    var span = document.getElementsByClassName("close")[0];
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+    var modal = document.getElementById("myModal6");
+    var butt = document.getElementById("mybutt6");
+    var span = document.getElementsByClassName("close")[0];
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+    var modal = document.getElementById("myModal7");
+    var butt = document.getElementById("mybutt7");
+    var span = document.getElementsByClassName("close")[0];
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+    var modal = document.getElementById("myModal8");
+    var butt = document.getElementById("mybutt8");
+    var span = document.getElementsByClassName("close")[0];
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+    var modal = document.getElementById("myModal9");
+    var butt = document.getElementById("mybutt9");
+    var span = document.getElementsByClassName("close")[0];
+
+    butt.onclick = function(){
+        modal.style.display = "block";
+    };
+
+    span.onclick = function (){
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event){
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+});
