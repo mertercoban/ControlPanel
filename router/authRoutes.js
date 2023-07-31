381139aa91dde81a5531f2e62398f92f4e2a0f5f
Merter <90144140+Sugarchild@users.noreply.github.com>
2023-07-31 17:07:10 +0300

    update files
    bug fixed

--- router/authRoutes.js
+++ router/authRoutes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const authController = require('../controllers/authController')
 
-router.get('/login',authController.login_get)
-router.post('/login',authController.login_post)
+router.get('/login.html',authController.login_get)
+router.post('/login.html',authController.login_post)
 router.get('/signup',authController.signup_get)
 router.post('/signup',authController.signup_post)
 router.get('/logout',authController.logout_get)
