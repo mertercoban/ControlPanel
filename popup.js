f007cb7dbbd6af3749ff4b215cbc0e77bd0aa7cd
Merter <90144140+Sugarchild@users.noreply.github.com>
2023-08-01 12:04:24 +0300

    update buttons

--- popup.js
+++ popup.js
@@ -1,151 +1,27 @@
 
-
 document.addEventListener("DOMContentLoaded", function() {
-    var modal = document.getElementById("myModal1");
-    var butt = document.getElementById("mybutt1");
-    var span = document.getElementsByClassName("close")[0];
-    butt.onclick = function(){
+        
+    for(var i=1;i<10;i++){
+        var modal;
+        var butt;
+        var span;
+    modal = document.getElementById("myModal"+i);
+    butt = document.getElementById("mybutt"+i);
+    span = document.getElementsByClassName("close")[0];
+    
+    (butt).onclick = function(){
         modal.style.display = "block";
     };
-
-    span.onclick = function (){
+    (span).onclick = function (){
         modal.style.display = "none";
     };
 
-    window.onclick = function (event){
+    (window).onclick = function (event){
         if (event.target == modal) {
             modal.style.display = "none";
         }
     };
-    var modal = document.getElementById("myModal2");
-    var butt = document.getElementById("mybutt2");
-    var span = document.getElementsByClassName("close")[0];
-    butt.onclick = function(){
-        modal.style.display = "block";
-    };
-
-    span.onclick = function (){
-        modal.style.display = "none";
-    };
-
-    window.onclick = function (event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    var modal = document.getElementById("myModal3");
-    var butt = document.getElementById("mybutt3");
-    var span = document.getElementsByClassName("close")[0];
-    butt.onclick = function(){
-        modal.style.display = "block";
-    };
-
-    span.onclick = function (){
-        modal.style.display = "none";
-    };
-
-    window.onclick = function (event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    var modal = document.getElementById("myModal4");
-    var butt = document.getElementById("mybutt4");
-    var span = document.getElementsByClassName("close")[0];
-    butt.onclick = function(){
-        modal.style.display = "block";
-    };
-
-    span.onclick = function (){
-        modal.style.display = "none";
-    };
-
-    window.onclick = function (event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    var modal = document.getElementById("myModal5");
-    var butt = document.getElementById("mybutt5");
-    var span = document.getElementsByClassName("close")[0];
-    butt.onclick = function(){
-        modal.style.display = "block";
-    };
-
-    span.onclick = function (){
-        modal.style.display = "none";
-    };
-
-    window.onclick = function (event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    var modal = document.getElementById("myModal6");
-    var butt = document.getElementById("mybutt6");
-    var span = document.getElementsByClassName("close")[0];
-    butt.onclick = function(){
-        modal.style.display = "block";
-    };
-
-    span.onclick = function (){
-        modal.style.display = "none";
-    };
-
-    window.onclick = function (event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    var modal = document.getElementById("myModal7");
-    var butt = document.getElementById("mybutt7");
-    var span = document.getElementsByClassName("close")[0];
-    butt.onclick = function(){
-        modal.style.display = "block";
-    };
-
-    span.onclick = function (){
-        modal.style.display = "none";
-    };
-
-    window.onclick = function (event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    var modal = document.getElementById("myModal8");
-    var butt = document.getElementById("mybutt8");
-    var span = document.getElementsByClassName("close")[0];
-    butt.onclick = function(){
-        modal.style.display = "block";
-    };
-
-    span.onclick = function (){
-        modal.style.display = "none";
-    };
-
-    window.onclick = function (event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
-    var modal = document.getElementById("myModal9");
-    var butt = document.getElementById("mybutt9");
-    var span = document.getElementsByClassName("close")[0];
-
-    butt.onclick = function(){
-        modal.style.display = "block";
-    };
-
-    span.onclick = function (){
-        modal.style.display = "none";
-    };
-
-    window.onclick = function (event){
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    };
+}
 });
 
 const authRoutes = require('./router/authRoutes')
