22601ed0346516ca2f04aa14c2272babcc1767e7
Merter Çoban <90144140+mertercoban@users.noreply.github.com>
2024-03-22 00:18:12 +0300

    updated source path

---
+++ src/popup.js
@@ -0,0 +1,23 @@
+
+
+var isInfoVisible = {};
+function showAll(person) {
+    var content = document.getElementById('myModal' + person);
+    if (isInfoVisible[person]) {
+        content.style.display = 'none';
+        isInfoVisible[person] = false;
+    }
+    else {
+        content.style.display = 'block';
+        isInfoVisible[person] = true;
+    }
+}
+
+const authRoutes = require('../router/authRoutes')
+const cookieParser = require('cookie-parser')
+const {requireAuth, checkUser} = require('../middlewares/authMiddleware')
+
+app.get('*', checkUser)
+app.use('/', authRoutes)
+app.use('/login',requireAuth,adminRoutes)
+app.use(cookieParser())
